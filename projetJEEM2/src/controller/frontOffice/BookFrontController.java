138850910c6d0a2122127394d78b5e246e46a5a9
cdubois <clement.dubois77@gmail.com>
2013-06-17 01:07:11 +0200

    front liste et show book

--- projetJEEM2/src/controller/frontOffice/BookFrontController.java
+++ projetJEEM2/src/controller/frontOffice/BookFrontController.java
@@ -1,35 +1,66 @@
 package controller.frontOffice;
 
 import java.io.Serializable;
-import java.util.Hashtable;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.slf4j.Logger;
 
-import ejb.CategoryService;
+import ejb.BookService;
+import entities.Book;
 import entities.Category;
-
-import beans.MessageBean;
 
 @Named
 @SessionScoped
 public class BookFrontController implements Serializable {
 	@Inject
 	private Logger log;
-	@Inject
-	private Category category;
 	@EJB
 	private BookService bookService;
+	@Inject
+	@Named
+	private List<Book> books;
+	@Named
+	private Book book;
 	
-	public List getBooks(Category c){
+	public String index(Category c){
 		if(c != null){
-			category = c;
+			System.out.println(c.getTitle());
+			books = c.getBooks();
+			for(Book b : books){
+				System.out.println(b.getTitle());
+			}
+		}else{
+			System.out.println("pas categori");
+			books = (List<Book>) bookService.findAll();
 		}
+		return "/frontOffice/books/index";
+	}
+	
+	public String showBook(Book b){
+		this.setBook(b);
+		return "/frontOffice/books/show";
+	}
+	
+	
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
 	}
 }
